38f342a003c9791f67f77537ed0e3e8e035b7968
Gilberto Cezar de Almeida <gibalmeida@gmail.com>
2021-10-15 17:04:40 -0300

    Implementa uma API GraphQL

---
+++ graphql-server/src/graphql_schema.rs
@@ -0,0 +1,584 @@
+extern crate dotenv;
+
+use chrono::NaiveDate;
+use diesel::mysql::MysqlConnection;
+use diesel::prelude::*;
+
+use juniper::{EmptyMutation, EmptySubscription, FieldError, RootNode};
+
+use bigdecimal::{BigDecimal, ToPrimitive};
+
+#[derive(Queryable)]
+struct CNAE {
+    id: u32,
+    nome: String,
+}
+
+#[graphql_object(description = "CNAE - Classificação Nacional de Atividade Econômica")]
+impl CNAE {
+    pub fn id(&self) -> f64 {
+        self.id.into()
+    }
+
+    pub fn nome(&self) -> &String {
+        &self.nome
+    }
+}
+
+#[derive(Queryable)]
+struct NaturezaJuridica {
+    id: u16,
+    nome: String,
+}
+
+#[juniper::graphql_object(description = "Natureza Jurídica")]
+impl NaturezaJuridica {
+    pub fn id(&self) -> i32 {
+        self.id.into()
+    }
+
+    pub fn nome(&self) -> &String {
+        &self.nome
+    }
+}
+
+#[derive(Queryable)]
+struct Municipio {
+    id: u16,
+    nome: String,
+}
+
+#[graphql_object(description = "Município")]
+impl Municipio {
+    pub fn id(&self) -> i32 {
+        self.id.into()
+    }
+
+    pub fn nome(&self) -> &String {
+        &self.nome
+    }
+}
+
+#[derive(Queryable)]
+struct Pais {
+    id: u16,
+    nome: String,
+}
+
+#[graphql_object(description = "País")]
+impl Pais {
+    pub fn id(&self) -> i32 {
+        self.id.into()
+    }
+
+    pub fn nome(&self) -> &String {
+        &self.nome
+    }
+}
+
+#[derive(Queryable)]
+struct QualificacaoDeSocio {
+    id: u8,
+    nome: String,
+}
+
+#[graphql_object(description = "Qualificação de Sócio")]
+impl QualificacaoDeSocio {
+    pub fn id(&self) -> i32 {
+        self.id.into()
+    }
+
+    pub fn nome(&self) -> &String {
+        &self.nome
+    }
+}
+
+#[derive(Queryable)]
+struct MotivoDeSituacaoCadastral {
+    id: u8,
+    nome: String,
+}
+
+#[graphql_object(description = "Motivo de Situação Cadastral")]
+impl MotivoDeSituacaoCadastral {
+    pub fn id(&self) -> i32 {
+        self.id.into()
+    }
+
+    pub fn nome(&self) -> &String {
+        &self.nome
+    }
+}
+
+#[derive(Queryable)]
+// #[primary_key(cnpj_basico)]
+struct Empresa {
+    cnpj_basico: String,
+    razao_social: String,
+    natureza_juridica: Option<u16>,
+    qualificacao_do_responsavel: Option<u8>,
+    capital_social: Option<BigDecimal>,
+    porte: Option<String>,
+    ente_federativo_responsavel: Option<String>,
+}
+
+#[juniper::graphql_object(context = Context, description="Uma Empresa registrada no CNPJ (Cadastro Nacional de Pessoas Jurídicas)")]
+impl Empresa {
+    pub fn cnpj_basico(&self) -> &String {
+        &self.cnpj_basico
+    }
+
+    pub fn razao_social(&self) -> &String {
+        &self.razao_social
+    }
+
+    pub fn natureza_juridica(
+        &self,
+        context: &Context,
+    ) -> Result<Option<NaturezaJuridica>, FieldError> {
+        match self.natureza_juridica {
+            Some(v) => {
+                use data_models::schema::naturezas_juridicas;
+                let connection = context.pool.clone().get()?;
+
+                Ok(Some(
+                    naturezas_juridicas::table
+                        .filter(naturezas_juridicas::id.eq(v))
+                        .first::<NaturezaJuridica>(&*connection)?,
+                ))
+            }
+            None => Ok(None),
+        }
+    }
+
+    pub fn qualificacao_do_responsavel(
+        &self,
+        context: &Context,
+    ) -> Result<Option<QualificacaoDeSocio>, FieldError> {
+        if let Some(v) = self.qualificacao_do_responsavel {
+            use data_models::schema::qualificacoes_de_socios;
+            let connection = context.pool.clone().get()?;
+            Ok(Some(
+                qualificacoes_de_socios::table
+                    .filter(qualificacoes_de_socios::id.eq(v))
+                    .first::<QualificacaoDeSocio>(&*connection)?,
+            ))
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub fn capital_social(&self) -> Option<f64> {
+        self.capital_social.as_ref()?.to_f64()
+    }
+
+    pub fn porte(&self) -> &Option<String> {
+        &self.porte
+    }
+
+    pub fn ente_federativo_responsavel(&self) -> &Option<String> {
+        &self.ente_federativo_responsavel
+    }
+}
+
+#[derive(Queryable)]
+struct Estabelecimento {
+    cnpj_basico: String,
+    cnpj_ordem: String,
+    cnpj_dv: String,
+    identificador_matriz_filial: String,
+    nome_fantasia: Option<String>,
+    situacao_cadastral: u8,
+    data_situacao_cadastral: Option<NaiveDate>,
+    motivo_situacao_cadastral: Option<u8>,
+    nome_da_cidade_no_exterior: Option<String>,
+    pais: Option<u16>,
+    data_de_inicio_da_atividade: Option<NaiveDate>,
+    cnae_fiscal_principal: Option<u32>,
+    cnae_fiscal_secundaria: Option<String>,
+    tipo_logradouro: Option<String>,
+    logradouro: Option<String>,
+    numero: Option<String>,
+    complemento: Option<String>,
+    bairro: Option<String>,
+    cep: Option<String>,
+    uf: Option<String>,
+    municipio: Option<u16>,
+    ddd1: Option<String>,
+    telefone1: Option<String>,
+    ddd2: Option<String>,
+    telefone2: Option<String>,
+    ddd_fax: Option<String>,
+    telefone_fax: Option<String>,
+    correio_eletronico: Option<String>,
+    situacao_especial: Option<String>,
+    data_situacao_especial: Option<NaiveDate>,
+}
+
+#[graphql_object(context = Context, description = "Um Estabelecimento pertencente a uma Empresa")]
+impl Estabelecimento {
+    pub fn cnpj_basico(&self) -> &String {
+        &self.cnpj_basico
+    }
+
+    pub fn cnpj_ordem(&self) -> &String {
+        &self.cnpj_ordem
+    }
+
+    pub fn cnpj_dv(&self) -> &String {
+        &self.cnpj_dv
+    }
+
+    pub fn identificador_matriz_filial(&self) -> &str {
+        match self.identificador_matriz_filial.as_str() {
+            "1" => "Matriz",
+            "2" => "Filial",
+            _ => "",
+        }
+    }
+
+    pub fn nome_fantasia(&self) -> &Option<String> {
+        &self.nome_fantasia
+    }
+
+    pub fn situacao_cadastral(&self) -> SituacaoCadastral {
+        match self.situacao_cadastral {
+            1 => SituacaoCadastral::NULA,
+            2 => SituacaoCadastral::ATIVA,
+            3 => SituacaoCadastral::SUSPENSA,
+            4 => SituacaoCadastral::INAPTA,
+            8 => SituacaoCadastral::BAIXADA,
+            _ => SituacaoCadastral::DESCONHECIDA,
+        }
+    }
+
+    pub fn data_situacao_cadastral(&self) -> &Option<NaiveDate> {
+        &self.data_situacao_cadastral
+    }
+
+    pub fn motivo_situacao_cadastral(
+        &self,
+        context: &Context,
+    ) -> Result<Option<MotivoDeSituacaoCadastral>, FieldError> {
+        match self.motivo_situacao_cadastral {
+            Some(v) => {
+                use data_models::schema::motivos_de_situacoes_cadastrais;
+                let connection = context.pool.clone().get()?;
+
+                Ok(Some(
+                    motivos_de_situacoes_cadastrais::table
+                        .filter(motivos_de_situacoes_cadastrais::id.eq(v))
+                        .first::<MotivoDeSituacaoCadastral>(&*connection)?,
+                ))
+            }
+            None => Ok(None),
+        }
+    }
+
+    pub fn nome_da_cidade_no_exterior(&self) -> &Option<String> {
+        &self.nome_da_cidade_no_exterior
+    }
+
+    pub fn pais(&self, context: &Context) -> Result<Option<Pais>, FieldError> {
+        match self.pais {
+            Some(v) => {
+                use data_models::schema::paises;
+                let connection = context.pool.clone().get()?;
+
+                Ok(Some(
+                    paises::table
+                        .filter(paises::id.eq(v))
+                        .first::<Pais>(&*connection)?,
+                ))
+            }
+            None => Ok(None),
+        }
+    }
+
+    pub fn data_de_inicio_da_atividade(&self) -> &Option<NaiveDate> {
+        &self.data_de_inicio_da_atividade
+    }
+
+    pub fn cnae_fiscal_principal(&self, context: &Context) -> Result<Option<CNAE>, FieldError> {
+        match self.cnae_fiscal_principal {
+            Some(v) => {
+                use data_models::schema::cnaes;
+                let connection = context.pool.clone().get()?;
+
+                Ok(Some(
+                    cnaes::table
+                        .filter(cnaes::id.eq(v))
+                        .first::<CNAE>(&*connection)?,
+                ))
+            }
+            None => Ok(None),
+        }
+    }
+
+    pub fn cnaes_fiscais_secundarias(&self, context: &Context) -> Result<Vec<CNAE>, FieldError> {
+        use data_models::schema::cnaes;
+        let connection = context.pool.clone().get()?;
+
+        let cnaes_fiscais_secundarias: Vec<u32> = match &self.cnae_fiscal_secundaria {
+            Some(v) => v
+                .to_string()
+                .split(",")
+                .map(|s| s.parse().unwrap())
+                .collect(),
+            _ => vec![],
+        };
+
+        let cnaes_fiscais_secundarias = cnaes::table
+            .filter(cnaes::id.eq_any(cnaes_fiscais_secundarias))
+            .load::<CNAE>(&*connection)?;
+
+        Ok(cnaes_fiscais_secundarias)
+    }
+
+    pub fn tipo_logradouro(&self) -> &Option<String> {
+        &self.tipo_logradouro
+    }
+
+    pub fn logradouro(&self) -> &Option<String> {
+        &self.logradouro
+    }
+
+    pub fn numero(&self) -> &Option<String> {
+        &self.numero
+    }
+
+    pub fn complemento(&self) -> &Option<String> {
+        &self.complemento
+    }
+
+    pub fn bairro(&self) -> &Option<String> {
+        &self.bairro
+    }
+
+    pub fn cep(&self) -> &Option<String> {
+        &self.cep
+    }
+
+    pub fn uf(&self) -> &Option<String> {
+        &self.uf
+    }
+
+    pub fn municipio(&self, context: &Context) -> Result<Option<Municipio>, FieldError> {
+        match self.municipio {
+            Some(v) => {
+                use data_models::schema::municipios;
+                let connection = context.pool.clone().get()?;
+
+                Ok(Some(
+                    municipios::table
+                        .filter(municipios::id.eq(v))
+                        .first::<Municipio>(&*connection)?,
+                ))
+            }
+            None => Ok(None),
+        }
+    }
+
+    pub fn ddd1(&self) -> &Option<String> {
+        &self.ddd1
+    }
+
+    pub fn telefone1(&self) -> &Option<String> {
+        &self.telefone1
+    }
+
+    pub fn ddd2(&self) -> &Option<String> {
+        &self.ddd2
+    }
+
+    pub fn telefone2(&self) -> &Option<String> {
+        &self.telefone2
+    }
+
+    pub fn ddd_fax(&self) -> &Option<String> {
+        &self.ddd_fax
+    }
+
+    pub fn telefone_fax(&self) -> &Option<String> {
+        &self.telefone_fax
+    }
+
+    pub fn correio_eletronico(&self) -> &Option<String> {
+        &self.correio_eletronico
+    }
+
+    pub fn situacao_especial(&self) -> &Option<String> {
+        &self.situacao_especial
+    }
+
+    pub fn data_situacao_especial(&self) -> &Option<NaiveDate> {
+        &self.data_situacao_especial
+    }
+}
+
+#[derive(GraphQLEnum)]
+#[graphql(description = "Situação Cadastral do Estabelecimento")]
+enum SituacaoCadastral {
+    NULA,
+    ATIVA,
+    SUSPENSA,
+    INAPTA,
+    BAIXADA,
+    DESCONHECIDA,
+}
+
+pub struct QueryRoot;
+
+#[juniper::graphql_object(context = Context)]
+impl QueryRoot {
+    fn api_version() -> &'static str {
+        "0.1"
+    }
+
+    fn cnae(context: &Context, id: String) -> Result<CNAE, FieldError> {
+        use data_models::schema::cnaes;
+        let connection = context.pool.clone().get()?;
+
+        Ok(cnaes::table
+            .filter(cnaes::id.eq(id.parse::<u32>()?))
+            .first::<CNAE>(&*connection)?)
+    }
+
+    fn cnaes(context: &Context) -> Result<Vec<CNAE>, FieldError> {
+        use data_models::schema::cnaes::dsl::*;
+
+        let connection = context.pool.clone().get()?;
+
+        Ok(cnaes.load::<CNAE>(&*connection)?)
+    }
+
+    fn natureza_juridica(context: &Context, id: String) -> Result<NaturezaJuridica, FieldError> {
+        use data_models::schema::naturezas_juridicas;
+        let connection = context.pool.clone().get()?;
+
+        Ok(naturezas_juridicas::table
+            .filter(naturezas_juridicas::id.eq(id.parse::<u16>()?))
+            .first::<NaturezaJuridica>(&*connection)?)
+    }
+
+    fn naturezas_juridicas(context: &Context) -> Result<Vec<NaturezaJuridica>, FieldError> {
+        use data_models::schema::naturezas_juridicas::dsl::*;
+        let connection = context.pool.clone().get()?;
+
+        Ok(naturezas_juridicas.load::<NaturezaJuridica>(&*connection)?)
+    }
+
+    fn municipio(context: &Context, id: String) -> Result<Municipio, FieldError> {
+        use data_models::schema::municipios;
+        let connection = context.pool.clone().get()?;
+
+        Ok(municipios::table
+            .filter(municipios::id.eq(id.parse::<u16>()?))
+            .first::<Municipio>(&*connection)?)
+    }
+
+    fn municipios(context: &Context) -> Result<Vec<Municipio>, FieldError> {
+        use data_models::schema::municipios::dsl::*;
+
+        let connection = context.pool.clone().get()?;
+
+        Ok(municipios.load::<Municipio>(&*connection)?)
+    }
+
+    fn pais(context: &Context, id: String) -> Result<Pais, FieldError> {
+        use data_models::schema::paises;
+        let connection = context.pool.clone().get()?;
+
+        Ok(paises::table
+            .filter(paises::id.eq(id.parse::<u16>()?))
+            .first::<Pais>(&*connection)?)
+    }
+
+    fn paises(context: &Context) -> Result<Vec<Pais>, FieldError> {
+        use data_models::schema::paises::dsl::*;
+
+        let connection = context.pool.clone().get()?;
+
+        Ok(paises.load::<Pais>(&*connection)?)
+    }
+
+    fn qualificacao_de_socio(
+        context: &Context,
+        id: String,
+    ) -> Result<QualificacaoDeSocio, FieldError> {
+        use data_models::schema::qualificacoes_de_socios;
+        let connection = context.pool.clone().get()?;
+
+        Ok(qualificacoes_de_socios::table
+            .filter(qualificacoes_de_socios::id.eq(id.parse::<u8>()?))
+            .first::<QualificacaoDeSocio>(&*connection)?)
+    }
+
+    fn qualificacoes_de_socios(context: &Context) -> Result<Vec<QualificacaoDeSocio>, FieldError> {
+        use data_models::schema::qualificacoes_de_socios::dsl::*;
+
+        let connection = context.pool.clone().get()?;
+
+        Ok(qualificacoes_de_socios.load::<QualificacaoDeSocio>(&*connection)?)
+    }
+
+    fn motivo_de_situacao_cadastral(
+        context: &Context,
+        id: String,
+    ) -> Result<MotivoDeSituacaoCadastral, FieldError> {
+        use data_models::schema::motivos_de_situacoes_cadastrais;
+        let connection = context.pool.clone().get()?;
+
+        Ok(motivos_de_situacoes_cadastrais::table
+            .filter(motivos_de_situacoes_cadastrais::id.eq(id.parse::<u8>()?))
+            .first::<MotivoDeSituacaoCadastral>(&*connection)?)
+    }
+
+    fn motivos_de_situacoes_cadastrais(
+        context: &Context,
+    ) -> Result<Vec<MotivoDeSituacaoCadastral>, FieldError> {
+        use data_models::schema::motivos_de_situacoes_cadastrais::dsl::*;
+        let connection = context.pool.clone().get()?;
+
+        Ok(motivos_de_situacoes_cadastrais.load::<MotivoDeSituacaoCadastral>(&*connection)?)
+    }
+
+    fn empresa(context: &Context, cnpj_basico: String) -> Result<Empresa, FieldError> {
+        use data_models::schema::empresas;
+        let connection = context.pool.clone().get()?;
+
+        Ok(empresas::table
+            .filter(empresas::cnpj_basico.eq(cnpj_basico))
+            .first::<Empresa>(&*connection)?)
+    }
+
+    fn estabelecimento(
+        context: &Context,
+        cnpj_completo: String,
+    ) -> Result<Estabelecimento, FieldError> {
+        use data_models::schema::estabelecimentos;
+        let connection = context.pool.clone().get()?;
+
+        Ok(estabelecimentos::table
+            .filter(estabelecimentos::cnpj_basico.eq(&cnpj_completo[..8]))
+            .filter(estabelecimentos::cnpj_ordem.eq(&cnpj_completo[8..12]))
+            .filter(estabelecimentos::cnpj_dv.eq(&cnpj_completo[12..]))
+            .first::<Estabelecimento>(&*connection)?)
+    }
+}
+
+pub struct Context {
+    pub pool: r2d2::Pool<r2d2_diesel::ConnectionManager<MysqlConnection>>,
+}
+
+impl juniper::Context for Context {}
+
+pub type Schema = RootNode<'static, QueryRoot, EmptyMutation<Context>, EmptySubscription<Context>>;
+
+pub fn create_schema() -> Schema {
+    Schema::new(
+        QueryRoot,
+        EmptyMutation::<Context>::new(),
+        EmptySubscription::<Context>::new(),
+    )
+}
--- graphql-server/src/main.rs
+++ graphql-server/src/main.rs
@@ -1,3 +1,62 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate juniper;
+
+extern crate r2d2;
+extern crate r2d2_diesel;
+use r2d2_diesel::ConnectionManager;
+
+use std::env;
+use diesel::MysqlConnection;
+use dotenv::dotenv;
+
+use rocket::{response::content, Rocket, State};
+
+use crate::graphql_schema::{Context, Schema};
+
+mod graphql_schema;
+
+#[rocket::get("/")]
+fn graphiql() -> content::Html<String> {
+    juniper_rocket::graphiql_source("/graphql", None)
+}
+
+#[rocket::get("/graphql?<request>")]
+fn get_graphql_handler(
+    context: &State<Context>,
+    request: juniper_rocket::GraphQLRequest,
+    schema: &State<Schema>,
+) -> juniper_rocket::GraphQLResponse {
+    request.execute_sync(&*schema, &*context)
+}
+
+#[rocket::post("/graphql", data = "<request>")]
+fn post_graphql_handler(
+    context: &State<Context>,
+    request: juniper_rocket::GraphQLRequest,
+    schema: &State<Schema>,
+) -> juniper_rocket::GraphQLResponse {
+    request.execute_sync(&*schema, &*context)
+}
+
+#[rocket::main]
+async fn main() {
+    dotenv().ok();
+
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set in .env");
+
+    let manager = ConnectionManager::<MysqlConnection>::new(db_url);
+    let pool = r2d2::Pool::builder().max_size(15).build(manager).unwrap();
+       
+    Rocket::build()
+        .manage(Context { pool })
+        .manage(graphql_schema::create_schema())
+        .mount(
+            "/",
+            rocket::routes![graphiql, get_graphql_handler, post_graphql_handler],
+        )
+        .launch()
+        .await
+        .expect("server to launch");
 }
